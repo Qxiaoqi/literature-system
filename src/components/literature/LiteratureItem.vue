2493fc65e421636943ff8fdfce124c299e397031
xiaoqi <308805548@qq.com>
2019-12-11 17:35:46 +0800

    抽离文献组件

---
+++ src/components/literature/LiteratureItem.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="literature-item">
+    <!-- 翻转效果 -->
+    <div class="flip">
+      <!-- 正面 -->
+      <div class="front">
+        <div class="front-top">
+          <div
+            class="collection"
+            :class="{
+              'collection-star': literature.star
+            }"
+          >
+            <img src="@/assets/star.png" alt="collection" />
+          </div>
+        </div>
+        <div class="front-bottom">
+          <p>{{ literature.title }}</p>
+        </div>
+      </div>
+      <!-- 反面 -->
+      <div class="back">
+        <ul>
+          <li class="back-item" @click="$emit('model-click', index)">详情</li>
+          <li class="back-item">下载</li>
+          <li class="back-item">移除</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LiteratureItem",
+  props: {
+    literature: Object,
+    index: Number
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.literature-item {
+  height: 250px;
+  width: 180px;
+  margin-bottom: 10px;
+  text-align: center;
+  border-radius: 4px;
+
+  .flip {
+    height: 100%;
+  }
+
+  .front {
+    height: 100%;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    box-shadow: rgba(39, 39, 39, 0.08) 3px 3px 5px;
+  }
+
+  .back {
+    width: 100%;
+    height: 100%;
+    // background-color: #2b85e4;
+    background-image: linear-gradient(135deg, #43cbff 10%, #9708cc 100%);
+    border-radius: 4px;
+    border: 1px solid #eee;
+    box-shadow: rgba(39, 39, 39, 0.08) 3px 3px 5px;
+
+    ul {
+      display: flex;
+      flex-flow: column nowrap;
+      justify-content: space-between;
+      height: 100%;
+      padding: 70px 0;
+      font-size: 18px;
+    }
+    .back-item {
+      color: #fff;
+      list-style: none;
+      cursor: pointer;
+    }
+  }
+
+  .front-top {
+    position: relative;
+    height: 100px;
+    background-color: #2b85e4;
+    // background-image: linear-gradient( 135deg, #6B73FF 10%, #000DFF 100%);
+    // background-image: linear-gradient( 135deg, #43CBFF 10%, #9708CC 100%);
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+
+    .collection {
+      position: absolute;
+      right: 20px;
+      bottom: -25px;
+      height: 50px;
+      width: 50px;
+      border-radius: 50%;
+      background-color: #c5c8ce;
+      box-shadow: rgba(41, 41, 41, 0.1) 1px 1px 3px;
+
+      img {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-top: -13px;
+        margin-left: -13px;
+        height: 26px;
+      }
+    }
+
+    .collection-star {
+      background-color: rgb(255, 209, 59);
+    }
+  }
+
+  .front-bottom {
+    padding: 30px 10px 20px;
+    font-weight: 700;
+  }
+}
+
+// 翻转效果
+.literature-item {
+  perspective: 1000;
+  transform-style: preserve-3d;
+}
+
+.flip {
+  position: relative;
+  transition: 0.6s;
+  transform-style: preserve-3d;
+}
+
+.front,
+.back {
+  position: absolute;
+  top: 0;
+  left: 0;
+  backface-visibility: hidden;
+}
+
+.front {
+  z-index: 2;
+}
+
+.back {
+  transform: rotateY(-180deg);
+}
+
+.literature-item:hover .flip {
+  transform: rotateY(180deg);
+}
+</style>
--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -53,33 +53,15 @@
       <!-- <Button type="primary" slot="extra" size="small">上传文献</Button> -->
       <div class="results-content">
         <div class="literature-content">
-          <!-- 单个文献 -->
-          <div class="literature-item" v-for="literature in literatureData" :key="literature.literatureId">
-            <!-- 翻转效果 -->
-            <div class="flip">
-              <!-- 正面 -->
-              <div class="front">
-                <div class="front-top">
-                  <div class="collection collection-star">
-                    <img src="../assets/star.png" alt="collection" />
-                  </div>
-                </div>
-                <div class="front-bottom">
-                  <p>{{ literature.title }}</p>
-                </div>
-              </div>
-              <!-- 反面 -->
-              <div class="back">
-                <ul>
-                  <li class="back-item" @click="literatureModel = true">详情</li>
-                  <li class="back-item">下载</li>
-                  <li class="back-item">移除</li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          <!-- 单个文献组件 -->
+          <LiteratureItem
+            v-for="(literature, index) in literatureData"
+            :key="literature.literatureId"
+            :literature="literature"
+            :index="index"
+            @model-click="modelClick"
+          ></LiteratureItem>
         </div>
-
 
         <!-- 分页 -->
         <div class="page-bar">
@@ -89,11 +71,21 @@
         <!-- 文献详情页弹窗 -->
         <Modal v-model="literatureModel" title="文献详情" footer-hide>
           <ul class="literature-model">
-            <li>标题：<span>基于门控递归单元神经网络的高速公路行程时间预测_刘松</span></li>
-            <li>作者：<span>刘松</span></li>
-            <li>来源：<span>China</span></li>
-            <li>发表时间：<span>2019.12.09</span></li>
-            <li>分类：<span>计算机科学</span></li>
+            <li>
+              标题：<span>{{ literatureModelData.title }}</span>
+            </li>
+            <li>
+              作者：<span>{{ literatureModelData.author }}</span>
+            </li>
+            <li>
+              来源：<span>{{ literatureModelData.source }}</span>
+            </li>
+            <li>
+              发表时间：<span>{{ literatureModelData.time }}</span>
+            </li>
+            <li>
+              分类：<span>{{ literatureModelData.category }}</span>
+            </li>
           </ul>
         </Modal>
       </div>
@@ -104,11 +96,12 @@
 <script>
 // @ is an alias to /src
 import { categoryData } from "@/data/category";
+import LiteratureItem from "@/components/literature/LiteratureItem.vue";
 
 export default {
   name: "personal",
   components: {
-    // HelloWorld
+    LiteratureItem
   },
   data() {
     return {
@@ -116,6 +109,13 @@ export default {
       checkAll: false,
       checkAllGroup: [1],
       literatureModel: false,
+      literatureModelData: {
+        title: null,
+        author: null,
+        source: null,
+        time: null,
+        category: null
+      },
       categoryData: categoryData,
       literatureData: [
         {
@@ -124,7 +124,8 @@ export default {
           author: "author1",
           source: "China",
           time: "2019.01.01",
-          category: 1 // 一一对应22个学科
+          category: 1, // 一一对应22个学科
+          star: true
         },
         {
           literatureId: 1002,
@@ -132,7 +133,8 @@ export default {
           author: "author1",
           source: "China",
           time: "2019.01.01",
-          category: 1 // 一一对应22个学科
+          category: 1, // 一一对应22个学科
+          star: true
         },
         {
           literatureId: 1003,
@@ -140,7 +142,8 @@ export default {
           author: "author1",
           source: "China",
           time: "2019.01.01",
-          category: 1 // 一一对应22个学科
+          category: 1, // 一一对应22个学科
+          star: true
         },
         {
           literatureId: 1004,
@@ -148,7 +151,8 @@ export default {
           author: "author1",
           source: "China",
           time: "2019.01.01",
-          category: 1 // 一一对应22个学科
+          category: 1, // 一一对应22个学科
+          star: true
         },
         {
           literatureId: 1005,
@@ -156,7 +160,8 @@ export default {
           author: "author1",
           source: "China",
           time: "2019.01.01",
-          category: 1 // 一一对应22个学科
+          category: 1, // 一一对应22个学科
+          star: true
         },
         {
           literatureId: 1006,
@@ -164,7 +169,8 @@ export default {
           author: "author1",
           source: "China",
           time: "2019.01.01",
-          category: 1 // 一一对应22个学科
+          category: 1, // 一一对应22个学科
+          star: true
         },
         {
           literatureId: 1007,
@@ -172,7 +178,8 @@ export default {
           author: "author1",
           source: "China",
           time: "2019.01.01",
-          category: 1 // 一一对应22个学科
+          category: 1, // 一一对应22个学科
+          star: true
         },
         {
           literatureId: 1011,
@@ -180,7 +187,8 @@ export default {
           author: "author1",
           source: "China",
           time: "2019.01.01",
-          category: 12 // 一一对应22个学科
+          category: 12, // 一一对应22个学科
+          star: true
         }
       ]
     };
@@ -229,6 +237,19 @@ export default {
         this.indeterminate = false;
         this.checkAll = false;
       }
+    },
+    modelClick(index) {
+      // console.log(this.literatureData[index]);
+      let data = this.literatureData[index];
+      for (let key in this.literatureModelData) {
+        this.literatureModelData[key] = data[key];
+      }
+      // id到 value转换
+      this.literatureModelData["category"] = this.categoryData[
+        this.literatureModelData.category - 1
+      ].value;
+      this.literatureModel = true;
+      // console.log(this.literatureModelData);
     }
   }
 };
@@ -276,119 +297,119 @@ export default {
 //   flex: 1;
 // }
 
-.literature-item {
-  height: 250px;
-  width: 180px;
-  margin-bottom: 10px;
-  text-align: center;
-  border-radius: 4px;
+// .literature-item {
+//   height: 250px;
+//   width: 180px;
+//   margin-bottom: 10px;
+//   text-align: center;
+//   border-radius: 4px;
 
-  .flip {
-    height: 100%;
-  }
+//   .flip {
+//     height: 100%;
+//   }
 
-  .front {
-    height: 100%;
-    border: 1px solid #eee;
-    border-radius: 4px;
-    box-shadow: rgba(39, 39, 39, 0.08) 3px 3px 5px;
-  }
+//   .front {
+//     height: 100%;
+//     border: 1px solid #eee;
+//     border-radius: 4px;
+//     box-shadow: rgba(39, 39, 39, 0.08) 3px 3px 5px;
+//   }
 
-  .back {
-    width: 100%;
-    height: 100%;
-    // background-color: #2b85e4;
-    background-image: linear-gradient(135deg, #43cbff 10%, #9708cc 100%);
-    border-radius: 4px;
-    border: 1px solid #eee;
-    box-shadow: rgba(39, 39, 39, 0.08) 3px 3px 5px;
+//   .back {
+//     width: 100%;
+//     height: 100%;
+//     // background-color: #2b85e4;
+//     background-image: linear-gradient(135deg, #43cbff 10%, #9708cc 100%);
+//     border-radius: 4px;
+//     border: 1px solid #eee;
+//     box-shadow: rgba(39, 39, 39, 0.08) 3px 3px 5px;
 
-    ul {
-      display: flex;
-      flex-flow: column nowrap;
-      justify-content: space-between;
-      height: 100%;
-      padding: 70px 0;
-      font-size: 18px;
-    }
-    .back-item {
-      color: #fff;
-      list-style: none;
-      cursor: pointer;
-    }
-  }
+//     ul {
+//       display: flex;
+//       flex-flow: column nowrap;
+//       justify-content: space-between;
+//       height: 100%;
+//       padding: 70px 0;
+//       font-size: 18px;
+//     }
+//     .back-item {
+//       color: #fff;
+//       list-style: none;
+//       cursor: pointer;
+//     }
+//   }
 
-  .front-top {
-    position: relative;
-    height: 100px;
-    background-color: #2b85e4;
-    // background-image: linear-gradient( 135deg, #6B73FF 10%, #000DFF 100%);
-    // background-image: linear-gradient( 135deg, #43CBFF 10%, #9708CC 100%);
-    border-top-left-radius: 4px;
-    border-top-right-radius: 4px;
+//   .front-top {
+//     position: relative;
+//     height: 100px;
+//     background-color: #2b85e4;
+//     // background-image: linear-gradient( 135deg, #6B73FF 10%, #000DFF 100%);
+//     // background-image: linear-gradient( 135deg, #43CBFF 10%, #9708CC 100%);
+//     border-top-left-radius: 4px;
+//     border-top-right-radius: 4px;
 
-    .collection {
-      position: absolute;
-      right: 20px;
-      bottom: -25px;
-      height: 50px;
-      width: 50px;
-      border-radius: 50%;
-      background-color: #c5c8ce;
-      box-shadow: rgba(41, 41, 41, 0.1) 1px 1px 3px;
+//     .collection {
+//       position: absolute;
+//       right: 20px;
+//       bottom: -25px;
+//       height: 50px;
+//       width: 50px;
+//       border-radius: 50%;
+//       background-color: #c5c8ce;
+//       box-shadow: rgba(41, 41, 41, 0.1) 1px 1px 3px;
 
-      img {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-top: -13px;
-        margin-left: -13px;
-        height: 26px;
-      }
-    }
+//       img {
+//         position: absolute;
+//         top: 50%;
+//         left: 50%;
+//         margin-top: -13px;
+//         margin-left: -13px;
+//         height: 26px;
+//       }
+//     }
 
-    .collection-star {
-      background-color: rgb(255, 209, 59);
-    }
-  }
+//     .collection-star {
+//       background-color: rgb(255, 209, 59);
+//     }
+//   }
 
-  .front-bottom {
-    padding: 30px 10px 20px;
-    font-weight: 700;
-  }
-}
+//   .front-bottom {
+//     padding: 30px 10px 20px;
+//     font-weight: 700;
+//   }
+// }
 
-// 翻转效果
-.literature-item {
-  perspective: 1000;
-  transform-style: preserve-3d;
-}
+// // 翻转效果
+// .literature-item {
+//   perspective: 1000;
+//   transform-style: preserve-3d;
+// }
 
-.flip {
-  position: relative;
-  transition: 0.6s;
-  transform-style: preserve-3d;
-}
+// .flip {
+//   position: relative;
+//   transition: 0.6s;
+//   transform-style: preserve-3d;
+// }
 
-.front,
-.back {
-  position: absolute;
-  top: 0;
-  left: 0;
-  backface-visibility: hidden;
-}
+// .front,
+// .back {
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   backface-visibility: hidden;
+// }
 
-.front {
-  z-index: 2;
-}
+// .front {
+//   z-index: 2;
+// }
 
-.back {
-  transform: rotateY(-180deg);
-}
+// .back {
+//   transform: rotateY(-180deg);
+// }
 
-.literature-item:hover .flip {
-  transform: rotateY(180deg);
-}
+// .literature-item:hover .flip {
+//   transform: rotateY(180deg);
+// }
 
 // 分页
 .page-bar {
